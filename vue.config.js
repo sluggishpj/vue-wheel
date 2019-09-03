c597979bb0bb1d126b5e08bfce85dbed2a997b98
sluggishpj <sluggishpj@qq.com>
2019-09-03 20:01:30 +0800

    add github deploy

--- vue.config.js
+++ vue.config.js
@@ -1,5 +1,5 @@
 module.exports = {
-  publicPath: process.env.NODE_ENV === 'production' ? '/' : '/',
+  publicPath: process.env.NODE_ENV === 'production' ? '/vue-wheel/' : '/',
   pwa: {
     name: 'vue-wheel',
     themeColor: '#4DBA87',
