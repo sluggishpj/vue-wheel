6168514b6dcb8e71562cf93e9ad2a262c4dc83f5
sluggishpj <sluggishpj@qq.com>
2019-09-03 18:20:46 +0800

    add pwa

--- vue.config.js
+++ vue.config.js
@@ -1,3 +1,10 @@
 module.exports = {
-  publicPath: process.env.NODE_ENV === 'production' ? '/vue-wheel' : '/'
+  publicPath: process.env.NODE_ENV === 'production' ? '/vue-wheel/' : '/',
+  pwa: {
+    name: 'vue-wheel',
+    themeColor: '#4DBA87',
+    msTileColor: '#000000',
+    appleMobileWebAppCapable: 'yes',
+    appleMobileWebAppStatusBarStyle: 'black'
+  }
 }
