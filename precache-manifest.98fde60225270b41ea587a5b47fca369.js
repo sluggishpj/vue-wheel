e4bb6a28b75f3bb8fdfa636d5317375eecdda147
Deployment Bot (from Travis CI) <deploy@travis-ci.org>
2021-02-02 11:29:52 +0000

    Deploy sluggishpj/vue-wheel to github.com/sluggishpj/vue-wheel.git:gh-pages

---
+++ precache-manifest.98fde60225270b41ea587a5b47fca369.js
@@ -0,0 +1,70 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "e283efc536bfb4601863",
+    "url": "css/app.39893a38.css"
+  },
+  {
+    "revision": "0034e0120bd7602bf42e",
+    "url": "css/chunk-Home-vue0.c475115a.css"
+  },
+  {
+    "revision": "fc0422ba8347cee8a705",
+    "url": "css/chunk-SwingNum-vue1.1aa4c9f3.css"
+  },
+  {
+    "revision": "457bc9b64a6dfb1a067f",
+    "url": "css/chunk-VirtualScrollElSelect-vue2~chunk-VirtualScrollElTableDirective-vue3.84bb20f7.css"
+  },
+  {
+    "revision": "834ae982d12d3e64419e",
+    "url": "css/chunk-VirtualScrollElTableDirective-vue3.aecf0e2b.css"
+  },
+  {
+    "revision": "535877f50039c0cb49a6196a5b7517cd",
+    "url": "fonts/element-icons.535877f5.woff"
+  },
+  {
+    "revision": "732389ded34cb9c52dd88271f1345af9",
+    "url": "fonts/element-icons.732389de.ttf"
+  },
+  {
+    "revision": "ca5972782a21c2bb71b2ed2633ff4c39",
+    "url": "index.html"
+  },
+  {
+    "revision": "e283efc536bfb4601863",
+    "url": "js/app.12e775e4.js"
+  },
+  {
+    "revision": "0034e0120bd7602bf42e",
+    "url": "js/chunk-Home-vue0.02fcb787.js"
+  },
+  {
+    "revision": "fc0422ba8347cee8a705",
+    "url": "js/chunk-SwingNum-vue1.872d3a70.js"
+  },
+  {
+    "revision": "4dae9cecddee36291047",
+    "url": "js/chunk-VirtualScrollElSelect-vue2.0f94e3f2.js"
+  },
+  {
+    "revision": "457bc9b64a6dfb1a067f",
+    "url": "js/chunk-VirtualScrollElSelect-vue2~chunk-VirtualScrollElTableDirective-vue3.ca0df14e.js"
+  },
+  {
+    "revision": "834ae982d12d3e64419e",
+    "url": "js/chunk-VirtualScrollElTableDirective-vue3.0e77523c.js"
+  },
+  {
+    "revision": "3c0bd21978d690df6ed2",
+    "url": "js/chunk-vendors.84875a64.js"
+  },
+  {
+    "revision": "59c666b1962520a2f6474dc6aa35ae80",
+    "url": "manifest.json"
+  },
+  {
+    "revision": "b6216d61c03e6ce0c9aea6ca7808f7ca",
+    "url": "robots.txt"
+  }
+]);
